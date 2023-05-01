b26bfbb70c59dbe4888d594434e9dfb79ebe16fc
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-05-01 12:42:44 +0300

    Add error handling and auth routes to server.js

--- server/server.js
+++ server/server.js
@@ -5,15 +5,29 @@ dotenv.config({ path: "./config/config.env" });
 import express from "express";
 import colors from "colors";
 import connectDB from "./config/db.js";
+import errorHandler from "./middlewares/errorHandler.js";
+
+// Route files
+import auth from "./routes/auth.js";
 
 const app = express();
 connectDB();
 
+app.use(express.json());
+
+// Mount Routes
+app.use("/api/v1/auth", auth);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () =>
-  console.log(`Server running on PORT:${PORT}`.yellow.bold)
-);
+const server = app.listen(PORT, () => {
+  console.log(`Server running on PORT:${PORT}`.yellow.bold);
+  console.log(
+    `Environment: `.yellow.bold + `${process.env.NODE_ENV}`.blue.bold
+  );
+});
 
 process.on("unhandledRejection", error => {
   console.log(`${error.stack}`.red);
