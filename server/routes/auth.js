158cf70cd3d3569781891921eb472530c63a84e3
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-05-03 13:07:16 +0300

    Add Google OAuth authentication and logout functionality to the authentication routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,31 @@
 import express from "express";
+import passport from "passport";
 
 const router = express.Router();
 
-import { login, signup } from "../controllers/users.js";
+import { getMe, logOut, login, signup } from "../controllers/auth.js";
 
 router.post("/signup", signup);
+
 router.post("/login", login);
+
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (req, res, next) => {
+    res.requestFrom = "Google";
+    next();
+  },
+  login
+);
+
+router.post("/logout", logOut);
+
+router.get("/me", getMe);
 
 export default router;
