99be7ace4d0c4114fe40098c39c8c68d03f2eb09
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-05-03 16:44:38 +0300

    Add case when wrong id provided

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -5,8 +5,9 @@ const errorHandler = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
 
-  console.log(err.stack.red);
+  console.log(`${err.stack}`.red);
 
+  // Invalid values
   if (err.name === "ValidationError") {
     const message = Object.values(err.errors)
       .map(err => err.message)
@@ -14,12 +15,18 @@ const errorHandler = (err, req, res, next) => {
     error = new ErrorResponse(message, 400);
   }
 
+  // Dublicate unique properties
   if (err.code === 11000) {
     const message = Object.keys(err.keyValue)
       .map(key => key + " has already been taken")
       .join(", ");
 
     error = new ErrorResponse(message, 400);
+  }
+
+  // Request wrong id
+  if (err.name === "CastError") {
+    error = new ErrorResponse(`Resource not found`, 404);
   }
 
   res.status(error.statusCode || 500).json({
