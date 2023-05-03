5625751e36ada628659afd13e89b062e43ee911f
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-05-03 10:58:08 +0300

    Add passport and cookieParser config & Add dist as static folder

--- server/server.js
+++ server/server.js
@@ -2,10 +2,18 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./config/config.env" });
 
+import path from "path";
 import express from "express";
 import colors from "colors";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import passport from "passport";
+import session from "express-session";
+
 import connectDB from "./config/db.js";
 import errorHandler from "./middlewares/errorHandler.js";
+import passportSetup from "./config/passport.js";
+passportSetup(passport);
 
 // Route files
 import auth from "./routes/auth.js";
@@ -14,9 +22,39 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
 // Mount Routes
-app.use("/api/v1/auth", auth);
+app.use("/api/auth", auth);
+
+// TODO: In production this will be client/dist
+app.use(
+  express.static(path.join(path.join(process.cwd(), "..", "testAPI", "dist")))
+);
+app.get("*", (req, res) =>
+  res.sendFile(
+    path.join(path.join(process.cwd(), "..", "testAPI", "dist", "index.html"))
+  )
+);
 
 app.use(errorHandler);
 
