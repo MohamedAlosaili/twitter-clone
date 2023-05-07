328509148d4006f05da4deb1357187d718e71425
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-05-07 16:12:43 +0300

    Add replies route to get tweet replies and add new reply

---
+++ server/routes/replies.js
@@ -0,0 +1,18 @@
+import express from "express";
+import multer from "multer";
+
+const router = express.Router({ mergeParams: true });
+
+import { getTweetReplies, addReply } from "../controllers/replies.js";
+
+import protect from "../middlewares/protect.js";
+import uploadMedia from "../middlewares/uploadMedia.js";
+
+const upload = multer();
+
+router
+  .route("/")
+  .get(getTweetReplies)
+  .post(protect, upload.array("media"), uploadMedia, addReply);
+
+export default router;
