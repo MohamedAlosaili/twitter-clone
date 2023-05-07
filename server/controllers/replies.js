774e3ca586b69e77e7abc05fac51622c8f506120
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-05-07 16:13:26 +0300

    Add GET and POST functionality to retrieve and add replies to tweets

---
+++ server/controllers/replies.js
@@ -0,0 +1,40 @@
+import Tweet from "../models/Tweet.js";
+import asyncHandler from "../middlewares/asyncHandler.js";
+import ErrorResponse from "../utils/errorResponse.js";
+
+// @desc    Get all replies for a tweet
+// @route   GET /api/tweets/:tweetId/replies
+// access   Public
+export const getTweetReplies = asyncHandler(async (req, res, next) => {
+  const { tweetId } = req.params;
+
+  const replies = await Tweet.find({ type: "reply", tweetId });
+
+  res.status(200).json({
+    success: true,
+    data: replies,
+  });
+});
+
+// @desc    Add a reply
+// @route   POST /api/tweets/:tweetId/replies
+// access   Private - anyone logged in
+export const addReply = asyncHandler(async (req, res, next) => {
+  const { tweetId } = req.params;
+
+  const tweet = await Tweet.findById(tweetId);
+
+  if (!tweet) {
+    return next(new ErrorResponse("Tweet not found", 404));
+  }
+
+  req.body.tweetId = tweetId;
+  req.body.authorId = req.user.id;
+  const reply = await Tweet.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: reply,
+    message: "Your tweet was sent.",
+  });
+});
