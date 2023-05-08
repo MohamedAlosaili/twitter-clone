43e079fb527ce363aeac683d42b968066a9eed98
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-05-08 10:05:52 +0300

    Add Follow model for users' relationships
    Validate relationship before adding it, if the user follows himself
    will throw an error also if the relationship already exists.
    Increase and decrease followers/following properties on adding/removing
    relationships

---
+++ server/models/Follow.js
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+import ErrorResponse from "../utils/errorResponse.js";
+
+const FollowSchema = new mongoose.Schema(
+  {
+    followerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    followingId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// I added pre(validate) hook instead of index, because index doesn't stop other
+// pre hooks which cause inconsistency in followers/following user properties number
+FollowSchema.pre("validate", async function (next) {
+  // Prevent user from follow himself
+  if (this.followerId.toString() === this.followingId.toString()) {
+    return next(
+      new ErrorResponse(
+        "Invalid follow request. User cannot follow himself",
+        400
+      )
+    );
+  }
+
+  const follow = await this.constructor.findOne({
+    followerId: this.followerId,
+    followingId: this.followingId,
+  });
+
+  if (follow) {
+    return next(new ErrorResponse("Duplicate follow relationship", 400));
+  }
+
+  next();
+});
+
+FollowSchema.statics.updateFollowStatus = async function (
+  followerId,
+  followingId,
+  number
+) {
+  await this.model("User").updateOne(
+    { _id: followerId },
+    { $inc: { following: number } }
+  );
+  await this.model("User").updateOne(
+    { _id: followingId },
+    { $inc: { followers: number } }
+  );
+};
+
+FollowSchema.pre("save", async function (next) {
+  return this.constructor.updateFollowStatus(
+    this.followerId,
+    this.followingId,
+    1
+  );
+});
+
+FollowSchema.pre("deleteOne", { document: true }, function (next) {
+  return this.constructor.updateFollowStatus(
+    this.followerId,
+    this.followingId,
+    -1
+  );
+});
+
+export default mongoose.model("Follow", FollowSchema);
