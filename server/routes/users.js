9434967b1ec086980060a41d79846f2a101418c9
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-05-10 17:27:12 +0300

    Add users route to handle requesting user's profile, follow lists, tweets, and likes

---
+++ server/routes/users.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const router = express.Router();
+
+import {
+  getFollowLists,
+  getUserLikes,
+  getUserMedia,
+  getUserProfile,
+  getUserTweets,
+} from "../controllers/users.js";
+
+import advancedResults from "../middlewares/advancedResults.js";
+
+router.get("/:id", getUserProfile);
+
+router.get("/:id/tweets", getUserTweets);
+
+router.get("/:id/media", getUserMedia, advancedResults);
+
+router.get("/:id/likes", getUserLikes, advancedResults);
+
+router.get("/:id/follow/:type", getFollowLists, advancedResults);
+
+export default router;
