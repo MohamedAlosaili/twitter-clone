7ff6a034024da3696ebacc25a182d4033af27983
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-05-10 13:52:41 +0300

    Add advancedResults middleware to the getTweets route in tweets.js

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -14,6 +14,7 @@ import {
   updateTweet,
 } from "../controllers/tweets.js";
 
+import advancedResults from "../middlewares/advancedResults.js";
 import protect from "../middlewares/protect.js";
 import uploadMedia from "../middlewares/uploadMedia.js";
 
@@ -24,7 +25,7 @@ const upload = multer();
 
 router
   .route("/")
-  .get(getTweets)
+  .get(getTweets, advancedResults)
   .post(protect, upload.array("media"), uploadMedia, addTweet);
 
 router
