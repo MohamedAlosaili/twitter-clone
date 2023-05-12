619fdabfc3f20f2693697d9a3d59b3ae2ff2613d
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-05-12 13:14:54 +0300

    Refactor to include aggregate query method and move pagination utility to the file
    Add support for aggregate pipeline in the query argument
    Prevent adding select on aggregate query method
    Refactor promise-calls to run in parallel with Promise.all()
    Move the pagination utility to this file

--- server/middlewares/advancedResults.js
+++ server/middlewares/advancedResults.js
@@ -1,5 +1,4 @@
 import asyncHandler from "./asyncHandler.js";
-import { getPageAndLimit, getPagination } from "../utils/pagination.js";
 
 const advancedResults = asyncHandler(async (req, res) => {
   let { page: pageNum, limit: limitNum, select, sort, ...filters } = req.query;
@@ -15,10 +14,14 @@ const advancedResults = asyncHandler(async (req, res) => {
     ? { ...req.controllerFilters }
     : {};
 
-  let query = req.model[req.queryMethod]({
-    ...filters,
-    ...controllerFilters,
-  });
+  const queryArg = req.aggregatePipeline
+    ? req.aggregatePipeline
+    : {
+        ...filters,
+        ...controllerFilters,
+      };
+
+  let query = req.model[req.queryMethod](queryArg);
 
   if (sort) {
     query = query.sort(sort);
@@ -26,7 +29,8 @@ const advancedResults = asyncHandler(async (req, res) => {
     query = query.sort("-createdAt");
   }
 
-  if (select) {
+  // aggregate.select() is not a function
+  if (select && !req.aggregatePipeline) {
     select = select.replace(/,/g, " ");
     query = query.select(select);
   }
@@ -37,8 +41,19 @@ const advancedResults = asyncHandler(async (req, res) => {
 
   const { page, limit, startIndex } = getPageAndLimit(pageNum, limitNum);
 
-  const total = await req.model.countDocuments(controllerFilters);
-  const result = await query.skip(startIndex).limit(limit);
+  const countDocuments = req.countDocuments
+    ? req.countDocuments
+    : req.model.countDocuments(controllerFilters);
+
+  let [result, total] = await Promise.all([
+    query.skip(startIndex).limit(limit),
+    countDocuments,
+  ]);
+
+  // aggregate() queryMethod will come with countDocuments result like this => [{ count: number }]
+  if (Array.isArray(total)) {
+    total = total[0]?.count ?? 0;
+  }
 
   const pagination = getPagination(page, limit, total, result.length);
 
@@ -49,5 +64,38 @@ const advancedResults = asyncHandler(async (req, res) => {
     total,
   });
 });
+
+const getPageAndLimit = (pageNum, limitNum) => {
+  let page = Math.abs(parseInt(pageNum)) || 1;
+  let limit = Math.abs(parseInt(limitNum)) || 25;
+
+  const startIndex = (page - 1) * limit;
+
+  return { startIndex, page, limit };
+};
+
+const getPagination = (page, limit, total, pageSize) => {
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  const pagination = {
+    next: false,
+    prev: false,
+    limit,
+    page,
+    pageSize,
+    totalPages: Math.ceil(total / limit),
+  };
+
+  if (endIndex < total) {
+    pagination.next = true;
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = true;
+  }
+
+  return pagination;
+};
 
 export default advancedResults;
