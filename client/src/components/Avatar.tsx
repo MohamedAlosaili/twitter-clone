1d322372cf5bcd9bed56a7c178262f4847068fcf
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-05-23 17:35:50 +0300

    Add Avatar component with props for displaying user avatar and username

---
+++ client/src/components/Avatar.tsx
@@ -0,0 +1,33 @@
+import getUserAvatar from "utils/getUserAvatar";
+
+interface Props {
+  avatarUrl: string;
+  username: string;
+  className?: string;
+  layer?: boolean;
+  onClick?: () => void;
+}
+
+const Avatar = ({
+  avatarUrl,
+  username,
+  className,
+  onClick,
+  layer = true,
+}: Props) => (
+  <div
+    className="relative cursor-pointer overflow-hidden rounded-full"
+    onClick={onClick}
+  >
+    <img
+      src={getUserAvatar(avatarUrl)}
+      alt={`${username} avatar`}
+      className={`aspect-square ${className}`}
+    />
+    {/* {layer && (
+      <div className="absolute inset-0 bg-black opacity-0 transition-opacity hover:opacity-25"></div>
+    )} */}
+  </div>
+);
+
+export default Avatar;
