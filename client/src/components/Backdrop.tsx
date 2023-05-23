c814b0ab7936affdf7b37fd55ddb8b5a369ad4e5
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-05-23 09:17:55 +0300

    Add transparent option & stopPropagation on children

--- client/src/components/Backdrop.tsx
+++ client/src/components/Backdrop.tsx
@@ -3,15 +3,24 @@ import { ReactNode } from "react";
 interface BackdropPropType {
   onClick: () => void;
   children: ReactNode;
+  transparent?: boolean;
 }
 
-const Backdrop = ({ onClick, children }: BackdropPropType) => {
+const Backdrop = ({
+  onClick,
+  children,
+  transparent = false,
+}: BackdropPropType) => {
   return (
     <div
       onClick={onClick}
-      className="fixed inset-0 z-50 bg-light-hover-2/10 dim:bg-dim-hover-2/10 dark:bg-dark-hover-2/10"
+      className={`fixed inset-0 z-50 ${
+        transparent
+          ? "bg-transparent"
+          : "bg-light-hover-2/10 dim:bg-dim-hover-2/10 dark:bg-dark-hover-2/10"
+      }`}
     >
-      {children}
+      <div onClick={e => e.stopPropagation()}>{children}</div>
     </div>
   );
 };
