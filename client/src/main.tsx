bbc1f1943621c8dbc77b73cdb8828b9f92ab652b
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-05-24 11:20:12 +0300

    Add React Router and context providers to main.tsx file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
+
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+import WindowContextProvider from "context/WindowContext";
+import UserContextProvider from "context/UserContent";
+import { getUserPrefer } from "utils/userPreference";
+
+// Pages
+import Home from "pages/Home";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="home" element={<Home />} />
+      <Route path="explore" element={<div>Search</div>} />
+      <Route path=":username" element={<div>Profile</div>} />
+    </Route>
+  )
+);
+
+getUserPrefer();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <UserContextProvider>
+      <WindowContextProvider>
+        <RouterProvider router={router} />
+      </WindowContextProvider>
+    </UserContextProvider>
   </React.StrictMode>
 );
